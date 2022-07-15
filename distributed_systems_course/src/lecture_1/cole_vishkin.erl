ed95a744f0adbcf396eebcfc6cb6e47dccf0eb91
BOC Dev <53903568+Qualia91@users.noreply.github.com>
2022-07-15 13:07:28 +0100

    gonna put this to rest for a bit

--- distributed_systems_course/src/lecture_1/cole_vishkin.erl
+++ distributed_systems_course/src/lecture_1/cole_vishkin.erl
@@ -9,6 +9,8 @@
 %%% Note:
 %%% This is a demonstration of the process involved, and not actually correct due to the 
 %%% fact you don't get shared memoory in erlang.
+%%% Also, the sending back to the ets table is just for debugging ease. The data structure 
+%%% is the processes we made along the way...
 %%%
 %%% @author boc_dev
 %%% @version 0.0.1
@@ -22,11 +24,18 @@
 %%% Exports and Definitions
 %%%=============================================================================
 
-% cole_vishkin:start().
+% cole_vishkin:start_list(3).
+% cole_vishkin:start_tree_graph(0).
 
 %% External API
 -export([
-    start_list/1
+    start_list/1,
+    start_tree_graph/0,
+    start_tree_graph_2/0,
+    start_tree_graph_3/0,
+    integer_to_bit_string/1,
+    pad_to_length/2,
+    cole_vishkin_colour_reduction/2
 ]).
 
 -define(SERVER, ?MODULE).
@@ -41,33 +50,151 @@ start_list(MaxVal) ->
     Tid = ets:new(linked_list_data, [ordered_set]),
 
     % Get length of maximum colour from max val in sequence
-    LastBitString = linked_list_node:integer_to_bit_string(MaxVal),
+    LastBitString = integer_to_bit_string(MaxVal),
     MaxBitStrLength = length(LastBitString),
 
     Self = self(),
 
-    {ok, FirstPid} = linked_list_node:start_link(0, null, MaxBitStrLength, Self),
-    ets:insert_new(Tid, {0, FirstPid, linked_list_node:pad_to_length(linked_list_node:integer_to_bit_string(0), MaxBitStrLength)}),
+    {ok, FirstPid} = distributed_node:start_link(0, MaxBitStrLength, Self),
+    ets:insert_new(Tid, {0, FirstPid, pad_to_length(integer_to_bit_string(0), MaxBitStrLength)}),
 
     % Start by making a linked list of processes
     LastPid = lists:foldl(
         fun(ItemIndex, PreviousPid) ->
-            {ok, Pid} = linked_list_node:start_link(ItemIndex, PreviousPid, MaxBitStrLength, Self),
-            ets:insert_new(Tid, {ItemIndex, Pid, linked_list_node:pad_to_length(linked_list_node:integer_to_bit_string(ItemIndex), MaxBitStrLength)}),
+            {ok, Pid} = distributed_node:start_link(ItemIndex, MaxBitStrLength, Self),
+            distributed_node:set_neighbours(Pid, [PreviousPid]),
+            Colour = pad_to_length(integer_to_bit_string(ItemIndex), MaxBitStrLength),
+            ets:insert_new(Tid, {ItemIndex, Pid, Colour}),
             Pid
         end,
         FirstPid,
         lists:seq(1, MaxVal)
     ),
 
-    % Make the linked list cyclical as required by algorithm
-    linked_list_node:set_previous_pid(FirstPid, LastPid),
+    distributed_node:set_neighbours(FirstPid, [LastPid]),
 
     iterate(Tid, MaxVal).
+
+start_tree_graph() ->
+
+% Input
+%     0
+%    / \
+%   1   2
+%  / \   \
+% 3   4   5
+
+    % Create ets table to store lined list information
+    Tid = ets:new(linked_list_data, [ordered_set]),
+
+    % Get length of maximum colour from max val in sequence
+    LastBitString = integer_to_bit_string(5),
+    MaxBitStrLength = length(LastBitString),
+
+    Pid0 = create_node(0, Tid, MaxBitStrLength),
+    Pid1 = create_node(1, Tid, MaxBitStrLength),
+    Pid2 = create_node(2, Tid, MaxBitStrLength),
+    Pid3 = create_node(3, Tid, MaxBitStrLength),
+    Pid4 = create_node(4, Tid, MaxBitStrLength),
+    Pid5 = create_node(5, Tid, MaxBitStrLength),
+    
+    distributed_node:set_neighbours(Pid0, [Pid3, Pid4, Pid5]),
+    distributed_node:set_neighbours(Pid1, [Pid0]),
+    distributed_node:set_neighbours(Pid2, [Pid0]),
+    distributed_node:set_neighbours(Pid3, [Pid1]),
+    distributed_node:set_neighbours(Pid4, [Pid1]),
+    distributed_node:set_neighbours(Pid5, [Pid2]),
+
+    iterate(Tid, 5).
+
+start_tree_graph_2() ->
+
+% Input
+%     0
+%    / \
+%   1   2
+%  / \ / \
+% 3   4   5
+
+    % Create ets table to store lined list information
+    Tid = ets:new(linked_list_data, [ordered_set]),
+
+    % Get length of maximum colour from max val in sequence
+    LastBitString = integer_to_bit_string(5),
+    MaxBitStrLength = length(LastBitString),
+
+    Pid0 = create_node(0, Tid, MaxBitStrLength),
+    Pid1 = create_node(1, Tid, MaxBitStrLength),
+    Pid2 = create_node(2, Tid, MaxBitStrLength),
+    Pid3 = create_node(3, Tid, MaxBitStrLength),
+    Pid4 = create_node(4, Tid, MaxBitStrLength),
+    Pid5 = create_node(5, Tid, MaxBitStrLength),
+    
+    distributed_node:set_neighbours(Pid0, [Pid3, Pid4, Pid5]),
+    distributed_node:set_neighbours(Pid1, [Pid0]),
+    distributed_node:set_neighbours(Pid2, [Pid0]),
+    distributed_node:set_neighbours(Pid3, [Pid1]),
+    distributed_node:set_neighbours(Pid4, [Pid1, Pid2]),
+    distributed_node:set_neighbours(Pid5, [Pid2]),
+
+    iterate(Tid, 5).
+
+start_tree_graph_3() ->
+
+% Input
+%     0
+%    / \
+%   1   2
+%  / \ / \
+% |   3   4
+%  \   \ /
+%   5   6
+%    \ /
+%     7
+    % Create ets table to store lined list information
+    Tid = ets:new(linked_list_data, [ordered_set]),
+
+    % Get length of maximum colour from max val in sequence
+    LastBitString = integer_to_bit_string(7),
+    MaxBitStrLength = length(LastBitString),
+
+    Pid0 = create_node(0, Tid, MaxBitStrLength),
+    Pid1 = create_node(1, Tid, MaxBitStrLength),
+    Pid2 = create_node(2, Tid, MaxBitStrLength),
+    Pid3 = create_node(3, Tid, MaxBitStrLength),
+    Pid4 = create_node(4, Tid, MaxBitStrLength),
+    Pid5 = create_node(5, Tid, MaxBitStrLength),
+    Pid6 = create_node(6, Tid, MaxBitStrLength),
+    Pid7 = create_node(7, Tid, MaxBitStrLength),
+    
+    distributed_node:set_neighbours(Pid0, [Pid7]),
+    distributed_node:set_neighbours(Pid1, [Pid0]),
+    distributed_node:set_neighbours(Pid2, [Pid0]),
+    distributed_node:set_neighbours(Pid3, [Pid1, Pid2]),
+    distributed_node:set_neighbours(Pid4, [Pid2]),
+    distributed_node:set_neighbours(Pid5, [Pid1]),
+    distributed_node:set_neighbours(Pid6, [Pid3, Pid4]),
+    distributed_node:set_neighbours(Pid7, [Pid5, Pid6]),
+
+    iterate(Tid, 7).
+
+integer_to_bit_string(Int) ->
+    integer_to_bit_string(Int, "").
+    
+pad_to_length(BitString, ExpectedLength) when length(BitString) < ExpectedLength ->
+    % Make a list of 0's of how short it is, and concatenate onto existing list
+    lists:concat([[$0 || _ <- lists:seq(1, ExpectedLength - length(BitString))], BitString]);
+pad_to_length(BitString, _ExpectedLength) ->
+    BitString.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+create_node(Index, Tid, MaxBitStrLength) ->
+    {ok, Pid} = distributed_node:start_link(Index, MaxBitStrLength, self()),
+    ets:insert_new(Tid, {Index, Pid, pad_to_length(integer_to_bit_string(Index), MaxBitStrLength)}),
+    Pid.
 
 recieve_iter_data(0, _Tid) ->
     [];
@@ -91,7 +218,7 @@ iterate(Tid, MaxVal) ->
     lager:info("Starting iteration"),
     ets:foldl(
         fun({_, Pid, _}, _) ->
-            linked_list_node:start_iteration(Pid)
+            distributed_node:start_iteration(Pid)
         end,
         0,
         Tid
@@ -100,17 +227,60 @@ iterate(Tid, MaxVal) ->
     % Now recieve all data back and sort to check
     _RetList = lists:sort(recieve_iter_data(MaxVal + 1, Tid)),
     
-    % Update all nodes with new colours
-    lager:info("Update node colours"),
-    ets:foldl(
-        fun({_, Pid, Colour}, _) ->
-            linked_list_node:set_colour(Pid, Colour)
-        end,
-        0,
-        Tid
-    ),
-    
     lager:info("Shared memory simulation object at end of iteration: ~p", [ets:tab2list(Tid)]).
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+integer_to_bit_string(0, String) ->
+    String;
+integer_to_bit_string(Int, String) ->
+    NextChar = case Int rem 2 of
+        0 ->
+            $0;
+        1 ->
+            $1
+    end,
+    integer_to_bit_string(Int div 2, [NextChar | String]).
+
+% This function, as the algorithm requires, assumes that the started colours are correct, and will fail if not.
+cole_vishkin_colour_reduction(CurrentColour, []) ->
+    CurrentColour;
+cole_vishkin_colour_reduction(CurrentColour, [NeighbourColour | Tl]) ->
+    UpdatedColour = pad_to_length(
+        convert_pair_to_binary_string(
+            lists:foldl(
+                fun({CurentColChar, PrevColChar}, Acc) ->
+                    compare_and_return(CurentColChar, PrevColChar, Acc)
+                end,
+                not_found,
+                lists:zip(CurrentColour, NeighbourColour)
+            )
+        ),
+        length(NeighbourColour)
+    ),
+    cole_vishkin_colour_reduction(UpdatedColour, Tl).
+
+% The point of this function is to find the character and index of the current string we are searching for
+% that doesnt match the previous string we are comparing against, yet as if we were reading it right to left.
+% It ios done that way to cut out the need for a lists:reverse.
+compare_and_return(CurentColChar, PrevColChar, _) when CurentColChar =/= PrevColChar ->
+    {0, CurentColChar};
+compare_and_return(_CurentColChar, _PrevColChar, not_found) ->
+    not_found;
+compare_and_return(_CurentColChar, _PrevColChar, {Counter, TargetCharacter}) ->
+    {Counter + 1, TargetCharacter}.
+
+convert_pair_to_binary_string({Index, TargetCharacter}) ->
+    integer_to_bit_string(Index) ++ [TargetCharacter].
+
+strip_leading_zeros([]) ->
+    "0";
+strip_leading_zeros([$0 | Tl]) ->
+    strip_leading_zeros(Tl);
+strip_leading_zeros([$1 | Tl]) ->
+    [$1 | Tl].
 
 %%%===================================================================
 %%% Tests
@@ -120,7 +290,61 @@ iterate(Tid, MaxVal) ->
 
 -include_lib("eunit/include/eunit.hrl").
 
-example_test() ->
-    ?assertEqual(true, true).
+integer_to_bit_string_test() ->
+
+    TestCases = [
+        {5,     "101"             },
+        {9,     "1001"            },
+        {11,    "1011"            },
+        {37862, "1001001111100110"}
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun(Input) -> integer_to_bit_string(Input) end).
+
+pad_to_length_test() ->
+
+    TestCases = [
+        {{"101",              16}, "0000000000000101"},
+        {{"1001",             16}, "0000000000001001"},
+        {{"1011",             16}, "0000000000001011"},
+        {{"1001001111100110", 16}, "1001001111100110"}
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun({Input, PaddingLength}) -> pad_to_length(Input, PaddingLength) end).
+
+cole_vishkin_colour_reduction_test() ->
+
+    TestCases = [
+        {{"0110010000", ["1010010000", "0010110000"]}, "0000000001"},
+        {{"0000000000", ["0000000001", "0000000010", "0000000100"]}, "0000000011"}
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun({StartColour, NeighbourColours}) -> cole_vishkin_colour_reduction(StartColour, NeighbourColours) end).
+
+convert_pair_to_binary_string_test() ->
+
+    TestCases = [
+        {{8, $1}, "10001"},
+        {{5, $0}, "1010" },
+        {{0, $1}, "1"    }
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun(Input) -> convert_pair_to_binary_string(Input) end).
+
+strip_leading_zeros_test() ->
+
+    TestCases = [
+        {"10001", "10001"},
+        {"00001", "1"    },
+        {"01010", "1010" },
+        {"0",     "0"    }
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun(Input) -> strip_leading_zeros(Input) end).
+
+%%%===================================================================
+%%% Internal Test Functions
+%%%===================================================================
 
 -endif.
+
---
+++ distributed_systems_course/src/lecture_1/distributed_node.erl
@@ -0,0 +1,276 @@
+%%%-----------------------------------------------------------------------------
+%%% @doc
+%%%
+%%% @author boc_dev
+%%% @version 0.0.1
+%%% @end
+%%%-----------------------------------------------------------------------------
+
+-module(distributed_node).
+-author(boc_dev).
+-behaviour(gen_server).
+
+%%%=============================================================================
+%%% Exports and Definitions
+%%%=============================================================================
+
+%% External API
+-export([
+    start_link/3,
+    start_iteration/1,
+    request_colour/2,
+    respond_colour/3,
+    set_neighbours/2
+]).
+
+%% Callbacks
+-export([
+    init/1, 
+    handle_call/3, 
+    handle_cast/2, 
+    handle_info/2,
+    terminate/2, 
+    code_change/3
+]).
+
+-define(SERVER, ?MODULE).
+
+-opaque neighbour_object() :: {pid(), string()}.
+
+%% Loop state
+-record(loop_state, {
+    index                  :: integer(),
+    neighbour_objects = [] :: list(neighbour_object()),
+    colour                 :: string(),
+    database_pid           :: pid()
+}).
+-opaque loop_state() :: loop_state.
+
+%%%=============================================================================
+%%% API
+%%%=============================================================================
+
+-spec start_link(integer(), integer(), pid()) -> {ok, pid()} | {error, {already_started, pid()}} | {error, Reason::any()}.
+start_link(Index, BitStringLength, DatabasePid) ->
+    gen_server:start_link(?MODULE, [Index, BitStringLength, DatabasePid], []).
+
+start_iteration(Pid) ->
+    gen_server:cast(Pid, iterate).
+
+request_colour(Pid, ReturnAddress) ->
+    gen_server:cast(Pid, {request_colour, ReturnAddress}).
+
+respond_colour(Pid, NeighbourColour, NeighbourPid) ->
+    gen_server:cast(Pid, {respond_colour, NeighbourColour, NeighbourPid}).
+
+set_neighbours(Pid, NeighbourPids) ->
+    gen_server:cast(Pid, {set_neighbours, NeighbourPids}).
+
+%%%=============================================================================
+%%% Gen Server Callbacks
+%%%=============================================================================
+
+-spec init(list()) -> {ok, loop_state()}.
+init([Index, BitStringLength, DatabasePid]) ->
+    StartingColour = cole_vishkin:pad_to_length(cole_vishkin:integer_to_bit_string(Index), BitStringLength),
+    LoopState = #loop_state{
+        index             = Index,
+        colour            = StartingColour,
+        database_pid      = DatabasePid
+    },
+    lager:debug("Starting node ~p with colour ~p", [Index, StartingColour]),
+    {ok, LoopState}.
+
+-spec handle_call(any(), pid(), loop_state()) -> {ok, any(), loop_state()}.
+handle_call(Request, _From, LoopState) ->
+    Reply = ok,
+    lager:debug("Other handle_call: ~p~n", [Request]),
+    {reply, Reply, LoopState}.
+
+-spec handle_cast(any(), loop_state()) -> {noreply, loop_state()}.
+handle_cast({set_neighbours, NeighbourPids}, LoopState) ->
+    {noreply, LoopState#loop_state{neighbour_objects = create_neighbour_objs(NeighbourPids)}};
+handle_cast({request_colour, ReturnAddress}, LoopState = #loop_state{colour = Colour}) ->
+    distributed_node:respond_colour(ReturnAddress, Colour, self()),
+    {noreply, LoopState};
+handle_cast({respond_colour, NeighbourColour, NeighbourPid}, LoopState = #loop_state{neighbour_objects = NeighbourObjs}) ->
+    {IsComplete, UpdatedNeighbourObjs} = update_and_check_complete(NeighbourColour, NeighbourPid, NeighbourObjs),
+    start_processing(IsComplete),
+    {noreply, LoopState#loop_state{neighbour_objects = UpdatedNeighbourObjs}};
+handle_cast(start_processing, LoopState = #loop_state{index = Index, colour = Colour, neighbour_objects = NeighbourObjs, database_pid = DatabasePid}) ->
+    NewColour = cole_vishkin:cole_vishkin_colour_reduction(Colour, get_neighbour_colours(NeighbourObjs)),
+    DatabasePid ! {update_colour, Index, NewColour},
+    {noreply, LoopState};
+handle_cast(iterate, LoopState = #loop_state{neighbour_objects = NeighbourObjs}) ->
+    {noreply, LoopState#loop_state{neighbour_objects = request_and_reset_neighbour_colours(NeighbourObjs)}};
+handle_cast(Msg, LoopState) ->
+    lager:debug("Other handle_cast: ~p~n", [Msg]),
+    {noreply, LoopState}.
+
+-spec handle_info(any(), loop_state()) -> {noreply, loop_state()}.
+handle_info(Info, LoopState) ->
+    lager:debug("Other handle_info: ~p~n", [Info]),
+    {noreply, LoopState}.
+
+-spec terminate(any(), loop_state()) -> ok.
+terminate(_Reason, _LoopState) ->
+    lager:debug("Terminating~n"),
+    ok.
+
+-spec code_change(any(), loop_state(), any()) -> {ok, loop_state()}.
+code_change(_OldVsn, LoopState, _Extra) ->
+    {ok, LoopState}.
+
+
+%%%=============================================================================
+%%% Internal
+%%%=============================================================================
+
+create_neighbour_objs(Pids) ->
+    lists:map(
+        fun(Pid) ->
+            {Pid, null}
+        end,
+        Pids).
+
+request_and_reset_neighbour_colours(NeighbourObjs) ->
+    lists:map(
+        fun({Pid, _}) ->
+            distributed_node:request_colour(Pid, self()),
+            {Pid, null}
+        end,
+        NeighbourObjs).
+
+update_and_check_complete(NeighbourColour, NeighbourPid, NeighbourObjs) ->
+    lists:foldl(
+        fun(Elem, Acc) -> 
+            update_and_check_iter(Elem, Acc, NeighbourColour, NeighbourPid) 
+        end,
+        {true, []},
+        NeighbourObjs).
+
+update_and_check_iter({Pid, IterColour}, {IsComplete, Acc}, NeighbourColour, Pid) ->
+    {IsComplete, [{Pid, NeighbourColour} | Acc]};
+update_and_check_iter({IterPid, null}, {IsComplete, Acc}, _NeighbourColour, _NeighbourPid) ->
+    {false, [{IterPid, null} | Acc]};
+update_and_check_iter({IterPid, IterColour}, {IsComplete, Acc}, _NeighbourColour, _NeighbourPid) ->
+    {IsComplete, [{IterPid, IterColour} | Acc]}.
+
+start_processing(true) ->
+    gen_server:cast(self(), start_processing);
+start_processing(false) ->
+    ok.
+
+get_neighbour_colours(NeighbourColours) ->
+    {_, Colours} = lists:unzip(NeighbourColours),
+    Colours.
+
+%%%===================================================================
+%%% Tests
+%%%===================================================================
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+create_neighbour_objs_test() ->
+    
+    TestCases = [
+        {[1,2,3], [{1,null}, {2,null}, {3,null}]},
+        {[], []}
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun(Input) -> create_neighbour_objs(Input) end).
+
+request_and_reset_neighbour_colours_test() ->
+    
+    meck:new(distributed_node, [unstick, passthrough]),
+    meck:expect(distributed_node, request_colour, fun(_, _) -> ok end),
+    
+    TestCases = [
+        {[{1,1}, {2,2}, {3,3}], [{1,null}, {2,null}, {3,null}]},
+        {[], []}
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun(Input) -> request_and_reset_neighbour_colours(Input) end),
+
+    meck:unload(distributed_node).
+
+update_and_check_iter_test() ->
+    
+    % Test cases
+    % 1) All are complete so far. Input matches. Returns is complete with updated array
+    % 2) It isnt complete so far. Input matches. Returns isnt complete with updated array
+    % 3) All are complete so far. Input doesnt match. Returns isnt complete with same array
+    % 4) It isnt complete so far. Input doesnt match. Returns isnt complete with same array
+
+    TestCases = [
+        {{{1, null}, {true,  [     ]}, 1, 1}, {true,  [{1, 1   }        ]}},
+        {{{1, null}, {false, [     ]}, 1, 1}, {false, [{1, 1   }        ]}},
+        {{{1, null}, {true,  [     ]}, 1, 2}, {false, [{1, null}        ]}},
+        {{{1, null}, {false, [     ]}, 1, 2}, {false, [{1, null}        ]}},
+
+        {{{1, null}, {false, [{2,2}]}, 1, 2}, {false, [{1, null}, {2, 2}]}},
+        {{{1, null}, {false, [{2,2}]}, 1, 1}, {false, [{1, 1   }, {2, 2}]}},
+        {{{1, null}, {true,  [{2,2}]}, 1, 2}, {false, [{1, null}, {2, 2}]}},
+        {{{1, null}, {true,  [{2,2}]}, 1, 1}, {true,  [{1, 1   }, {2, 2}]}},
+
+        {{{2, 2   }, {true,  [{1,1}]}, 1, 1}, {true,  [{2, 2   }, {1, 1}]}}
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun({IterPair, AccPair, NeighbourColour, Pid}) -> update_and_check_iter(IterPair, AccPair, NeighbourColour, Pid) end).
+
+update_and_check_complete_test() ->
+    
+    % Test cases
+    % 1) Inputs list with all missing and no matches. Returns not complete with the original array
+    % 2) Inputs list with all missing and one matches. Returns not complete with the updated array
+    % 2) Inputs list with one missing and one matches. Returns complete with the updated array
+
+    TestCases = [
+        {
+            {
+                0,
+                0,
+                [{1, null}, {2, null}, {3, null}]
+            },
+            {false, [{3, null}, {2, null}, {1, null}]}
+        },
+        {
+            {
+                1,
+                1,
+                [{1, null}, {2, null}, {3, null}]
+            },
+            {false, [{3, null}, {2, null}, {1, 1}]}
+        },
+        {
+            {
+                1,
+                1,
+                [{1, null}, {2, 2}, {3, 3}]
+            },
+            {true, [{3, 3}, {2, 2}, {1, 1}]}
+        },
+        {
+            {
+                1,
+                1,
+                [{1, null}, {2, null}, {3, 3}]
+            },
+            {false, [{3, 3}, {2, null}, {1, 1}]}
+        }
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun({NeighbourColour, NeighbourPid, NeighbourObjs}) -> update_and_check_complete(NeighbourColour, NeighbourPid, NeighbourObjs) end).
+
+get_neighbour_colours_test() ->
+    
+    TestCases = [
+        {[{a,1}, {b,2}, {c,3}], [1,2,3]},
+        {[], []}
+    ],
+
+    test_utilities:action_test_cases(TestCases, fun(Input) -> get_neighbour_colours(Input) end).
+
+-endif.
