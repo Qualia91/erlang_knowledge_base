1bec02e261a89855f9f0d9b069c49b299de13262
nickolas.wood <nickolas.wood@superbet.com>
2022-10-09 09:22:56 +0100

    properties pattern done, now to find a use...

--- properties_pattern/properties_node.erl
+++ properties_pattern/properties_node.erl
@@ -30,12 +30,10 @@
     code_change/3
 ]).
 
--define(SERVER, ?MODULE).
-
 %% Loop state
 -record(loop_state, {
-    parent = null,
-    env
+    parent = null :: null | pid(),
+    env           :: maps:map()
 }).
 
 -define(TIMEOUT, 5000).
@@ -72,6 +70,8 @@ handle_call({get_env, EnvPath}, _From, LoopState = #loop_state{env = Env, parent
     Resp = case find_val_in_map(Env, EnvPath) of
         {error, not_found} ->
             get_env(Parent, EnvPath);
+        Val when is_map(Val) ->
+            combine_envs(Val, get_env(Parent, EnvPath));
         Val ->
             Val
     end,
@@ -113,21 +113,172 @@ set_val_in_env(Env, [CurrentEnvKey | RestEnvKeys], Var) ->
     case maps:find(CurrentEnvKey, Env) of
         {ok, SubEnv} when is_map(SubEnv) ->
             Env#{CurrentEnvKey => set_val_in_env(SubEnv, RestEnvKeys, Var)};
-        error ->
-            {error, not_found}
+        _ ->
+            Env#{CurrentEnvKey => set_val_in_env(#{}, RestEnvKeys, Var)}
     end;
 set_val_in_env(Env, Key, Var) ->
     set_val_in_env(Env, [Key], Var).
+
+combine_envs(CurrentEnv, {error, not_found}) ->
+    CurrentEnv;
+combine_envs(CurrentEnv, ParentEnv) when is_map(ParentEnv) ->
+    maps:merge_with(fun env_combiner/3, ParentEnv, CurrentEnv);
+combine_envs(CurrentEnv, _ParentEnvVal) ->
+    CurrentEnv.
+
+env_combiner(_Key, Env1, Env2) when is_map(Env1), is_map(Env2) ->
+    maps:merge_with(fun env_combiner/3, Env1, Env2);
+env_combiner(_Key, _Env1, Env2) ->
+    Env2.
 
 %%%=============================================================================
 %%% Eunit Tests
 %%%=============================================================================
 
+% -include_lib("eunit/include/eunit.hrl").
+% c(properties_node).
+% properties_node:find_val_in_map_test().
+% properties_node:set_val_in_env_test().
+% properties_node:combine_envs_simple_test().
+% properties_node:combine_envs_not_found_test().
+% properties_node:combine_envs_complex_test().
+% properties_node:env_combiner_test().
+
 -ifdef(TEST).
 
 -include_lib("eunit/include/eunit.hrl").
 
-example_test() ->
-    ?assertEqual(true, true).
+find_val_in_map_test() ->
+
+    Env = #{
+        a => #{
+            b => 2,
+            c => 3
+        },
+        d => 4
+    },
+
+    ?assertEqual(Env, find_val_in_map(Env, [])),
+    ?assertEqual(#{b => 2, c => 3}, find_val_in_map(Env, a)),
+    ?assertEqual(2, find_val_in_map(Env, [a, b])),
+    ?assertEqual(3, find_val_in_map(Env, [a, c])),
+    ?assertEqual(4, find_val_in_map(Env, d)),
+    ?assertEqual({error, not_found}, find_val_in_map(Env, e)).
+
+set_val_in_env_test() ->
+
+    Env = #{
+        a => #{
+            b => 2,
+            c => 3
+        },
+        d => 4
+    },
+
+    ?assertEqual(#{a => #{b => 2, c => 3}, d => 5}, set_val_in_env(Env, d, 5)),
+    ?assertEqual(#{a => #{b => 5, c => 3}, d => 4}, set_val_in_env(Env, [a, b], 5)),
+    ?assertEqual(#{a => #{b => 2, c => #{e => 5}}, d => 4}, set_val_in_env(Env, [a, c, e], 5)),
+    ?assertEqual(#{a => #{b => 2, c => 3, d => #{e => 5}}, d => 4}, set_val_in_env(Env, [a, d, e], 5)).
+
+combine_envs_simple_test() ->
+
+    CurrentEnv = #{
+        a => #{
+            b => 2,
+            c => 3
+        },
+        d => 4
+    },
+
+    ParentEnv = #{
+        a => 5,
+        d => #{a => 5},
+        e => 4
+    },
+
+    ExpectedEnv = #{
+        a => #{
+            b => 2,
+            c => 3
+        },
+        d => 4,
+        e => 4
+    },
+
+    ?assertEqual(ExpectedEnv, combine_envs(CurrentEnv, ParentEnv)).
+
+combine_envs_not_found_test() ->
+
+    CurrentEnv = #{
+        a => #{
+            b => 2,
+            c => 3
+        },
+        d => 4
+    },
+
+    ParentEnv = {error, not_found},
+
+    ?assertEqual(CurrentEnv, combine_envs(CurrentEnv, ParentEnv)).
+
+combine_envs_complex_test() ->
+
+    CurrentEnv = #{
+        a => #{
+            b => 2,
+            c => 3,
+            h => #{
+                a => 1
+            }
+        },
+        d => 4
+    },
+
+    ParentEnv = #{
+        a => #{
+            f => 2,
+            g => 3,
+            h => #{
+                b => 1
+            }
+        }
+    },
+
+    ExpectedEnv = #{
+        a => #{
+            b => 2,
+            c => 3,
+            f => 2,
+            g => 3,
+            h => #{
+                a => 1,
+                b => 1
+            }
+        },
+        d => 4
+    },
+
+    ?assertEqual(ExpectedEnv, combine_envs(CurrentEnv, ParentEnv)).
+
+env_combiner_test() ->
+
+    CurrentEnv = #{
+        b => 2,
+        c => 3
+    },
+
+    ParentEnv = #{
+        f => 2,
+        g => 3
+    },
+
+    ExpectedEnv = #{
+        b => 2,
+        c => 3,
+        f => 2,
+        g => 3
+    },
+
+    ?assertEqual(ExpectedEnv, env_combiner(a, ParentEnv, CurrentEnv)).
 
 -endif.
--- properties_pattern/properties_pattern.erl
+++ properties_pattern/properties_pattern.erl
@@ -8,39 +8,51 @@
 -module(properties_pattern).
 -author("nickolaswood").
 
+-include_lib("eunit/include/eunit.hrl").
+
 %%%=============================================================================
 %%% Export and Defs
 %%%=============================================================================
 
 %% External API
 -export([
-    properties_patter_example/0
+properties_pattern_simple_example/0
 ]).
 
 % c(properties_node).
 % c(properties_pattern).
-% properties_pattern:properties_patter_example().
+% properties_pattern:properties_pattern_simple_example().
 
 %%%=============================================================================
 %%% API
 %%%=============================================================================
 
-properties_patter_example() ->
+properties_pattern_simple_example() ->
 
     GlobalEnvInit = #{
         a => 1,
         b => 2,
-        c => 3
+        c => 3,
+        x => #{
+            a => 1,
+            c => 3
+        }
     },
 
     Level1EnvInit = #{
         a => 2,
-        d => 3
+        d => 3,
+        x => #{
+            a => 2
+        }
     },
 
     Level2EnvInit = #{
         b => 7,
-        e => 3
+        e => 3,
+        x => #{
+            b => 1
+        }
     },
 
     {ok, GlobalEnv} = properties_node:start_link(GlobalEnvInit),
@@ -55,9 +67,34 @@ properties_patter_example() ->
     C = properties_node:get_env(Level2Env, c),
     D = properties_node:get_env(Level2Env, d),
     E = properties_node:get_env(Level2Env, e),
+    F = properties_node:get_env(Level2Env, f),
     G = properties_node:get_env(Level2Env, []),
+    H = properties_node:get_env(Level2Env, [x, a]),
+    I = properties_node:get_env(Level2Env, x),
 
-    io:format("~p, ~p, ~p, ~p, ~p, ~p~n", [A, B, C, D, E, G]).
+    ?assertEqual(2, A),
+    ?assertEqual(7, B),
+    ?assertEqual(3, C),
+    ?assertEqual(3, D),
+    ?assertEqual(3, E),
+    ?assertEqual({error, not_found}, F),
+
+    Expected = #{
+        a => 2,
+        b => 7,
+        c => 3,
+        d => 3,
+        e => 3,
+        x => #{
+            a => 2,
+            b => 1,
+            c => 3
+        }
+    },
+
+    ?assertEqual(Expected, G),
+    ?assertEqual(2, H),
+    ?assertEqual(#{a => 2, b => 1, c => 3}, I).
     
 
 %%%=============================================================================
